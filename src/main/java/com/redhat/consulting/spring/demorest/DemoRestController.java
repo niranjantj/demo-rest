f62a2d9f17cac6d2f52367fb42abae06fe1c0611
David Sancho <dsanchor@redhat.com>
2017-09-12 15:42:35 +0200

    Added simple rest method

---
+++ src/main/java/com/redhat/consulting/spring/demorest/DemoRestController.java
@@ -0,0 +1,32 @@
+package com.redhat.consulting.spring.demorest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Simple Rest API 
+ * 
+ * @author dsancho
+ *
+ */
+@RestController
+public class DemoRestController {
+	
+	@RequestMapping(method=RequestMethod.GET, path="/demo")
+	Collection<Integer> getOrderedRandomList() {
+		Collection<Integer> collection = new ArrayList<>();
+		int number = 0;
+		for (int i=0; i<5; i++) {
+			number = number + new Random().nextInt(100);
+			collection.add(number);
+		}
+		return collection;
+	}
+	
+
+}
